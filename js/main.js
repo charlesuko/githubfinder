75b32780e1d089d1815e55d7fd7283ed23be9854
Charles Uko <charles4genius@yahoo.com>
2017-04-04 11:53:00 +0100

    add extra functionality

--- js/main.js
+++ js/main.js
@@ -11,7 +11,36 @@ $(document).ready(function(){
 			}  
 		}).done(function(user){
 			$('#profile').html(`
-				${user.name}`);
+					<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">${user.name}</h3>
+  </div>
+  <div class="panel-body">
+    <div class="row">
+    	<div class="col-md-3">
+    		<img class="thumbnail avatar" src="${user.avatar_url}">
+    		<a class="btn btn-block btn-primary" target="_blank"  href="${user.html_url}">View profile</a>
+    	</div>
+    	<div class="col-md-9">
+    		<span class="label label-default">Public Repos: ${user.public_repos}</span>
+			<span class="label label-primary">Public Gists: ${user.public_gists}</span>
+			<span class="label label-success">Public Followers: ${user.followers}</span>
+			<span class="label label-info">Following: ${user.following}</span>
+			<br> <br>
+			<ul class="list-group">
+				<li class="list-group-item">Company: ${user.company}</li>
+				<li class="list-group-item">Website: ${user.blog}</li>
+				<li class="list-group-item">Location: ${user.location}</li>
+				<li class="list-group-item">Member Since: ${user.created_at}</li>
+
+
+
+    	</div>
+    	
+    </div>
+  </div>
+</div>
+				`);
 		});
 	});
 });
